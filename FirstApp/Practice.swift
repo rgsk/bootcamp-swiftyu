72774422e76115c44235fe26b27fcbd5bf2d3ef7
rgsk <guptarahul70322@gmail.com>
2023-07-12 01:26:08 +0530

    2. Text

---
+++ FirstApp/Practice.swift
@@ -0,0 +1,20 @@
+//
+//  Practice.swift
+//  FirstApp
+//
+//  Created by Rahul Gupta on 12/07/23.
+//
+
+import SwiftUI
+
+struct Practice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Practice_Previews: PreviewProvider {
+    static var previews: some View {
+        Practice()
+    }
+}
