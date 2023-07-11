8f27b51d82eda86fcf67342e0a097c39e9207c2c
rgsk <guptarahul70322@gmail.com>
2023-07-12 00:18:34 +0530

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Rahul Gupta on 12/07/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
